75603dce59880b84cfa8c4f3fe02730856f872bb
jinvkyen <ayenjtt.1902@gmail.com>
2025-04-10 12:07:38 +0800

    WokroutContext/DELETE-done

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -1,6 +1,21 @@
 import { formatDistanceToNow } from "date-fns";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
 const WorkoutDetails = ({ workout }) => {
+
+  const { dispatch } = useWorkoutsContext();
+
+  const handleDelete = async () => {
+    const response = await fetch("/api/workouts/" + workout._id, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: 'DELETE_WORKOUT', payload: json });
+    }
+  };
+
   return (
     <div className='workout-details'>
       <h4 className='font-bold text-xl'>{workout.title}</h4>
@@ -11,6 +26,10 @@ const WorkoutDetails = ({ workout }) => {
         <strong>Loads(kg):</strong> {workout.loads}
       </p>
       <p>Created {formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p>
+
+      <span onClick={handleDelete} className=''>
+        Delete
+      </span>
     </div>
   );
 };
